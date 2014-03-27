44f734fb7c84d692539c1ee16b07b2acf06cd5b0
Krozark <maxime.barbier1991@gmail.com>
2014-03-27 16:30:10 +0100

    ..

--- include/Monitoring/Win/_mingw_unicode.h
+++ include/Monitoring/Win/_mingw_unicode.h
@@ -1,33 +1,33 @@
-1	/**
-2	 * This file has no copyright assigned and is placed in the Public Domain.
-3	 * This file is part of the w64 mingw-runtime package.
-4	 * No warranty is given; refer to the file DISCLAIMER.PD within this package.
-5	 */
-6
-7	#if !defined(_INC_CRT_UNICODE_MACROS)
-8	/* _INC_CRT_UNICODE_MACROS defined based on UNICODE flag */
-9
-10	#if defined(UNICODE)
-11	# define _INC_CRT_UNICODE_MACROS 1
-12	# define __MINGW_NAME_AW(func) func##W
-13	# define __MINGW_NAME_AW_EXT(func,ext) func##W##ext
-14	# define __MINGW_NAME_UAW(func) func##_W
-15	# define __MINGW_NAME_UAW_EXT(func,ext) func##_W_##ext
-16	# define __MINGW_STRING_AW(str) L##str  /* same as TEXT() from winnt.h */
-17	# define __MINGW_PROCNAMEEXT_AW "W"
-18	#else
-19	# define _INC_CRT_UNICODE_MACROS 2
-20	# define __MINGW_NAME_AW(func) func##A
-21	# define __MINGW_NAME_AW_EXT(func,ext) func##A##ext
-22	# define __MINGW_NAME_UAW(func) func##_A
-23	# define __MINGW_NAME_UAW_EXT(func,ext) func##_A_##ext
-24	# define __MINGW_STRING_AW(str) str     /* same as TEXT() from winnt.h */
-25	# define __MINGW_PROCNAMEEXT_AW "A"
-26	#endif
-27
-28	#define __MINGW_TYPEDEF_AW(type)        \
-29	    typedef __MINGW_NAME_AW(type) type;
-30	#define __MINGW_TYPEDEF_UAW(type)       \
-31	    typedef __MINGW_NAME_UAW(type) type;
-32
-33	#endif /* !defined(_INC_CRT_UNICODE_MACROS) */
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the w64 mingw-runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#if !defined(_INC_CRT_UNICODE_MACROS)
+/* _INC_CRT_UNICODE_MACROS defined based on UNICODE flag */
+
+#if defined(UNICODE)
+# define _INC_CRT_UNICODE_MACROS 1
+# define __MINGW_NAME_AW(func) func##W
+# define __MINGW_NAME_AW_EXT(func,ext) func##W##ext
+# define __MINGW_NAME_UAW(func) func##_W
+# define __MINGW_NAME_UAW_EXT(func,ext) func##_W_##ext
+# define __MINGW_STRING_AW(str) L##str  /* same as TEXT() from winnt.h */
+# define __MINGW_PROCNAMEEXT_AW "W"
+#else
+# define _INC_CRT_UNICODE_MACROS 2
+# define __MINGW_NAME_AW(func) func##A
+# define __MINGW_NAME_AW_EXT(func,ext) func##A##ext
+# define __MINGW_NAME_UAW(func) func##_A
+# define __MINGW_NAME_UAW_EXT(func,ext) func##_A_##ext
+# define __MINGW_STRING_AW(str) str     /* same as TEXT() from winnt.h */
+# define __MINGW_PROCNAMEEXT_AW "A"
+#endif
+
+#define __MINGW_TYPEDEF_AW(type)        \
+    typedef __MINGW_NAME_AW(type) type;
+#define __MINGW_TYPEDEF_UAW(type)       \
+    typedef __MINGW_NAME_UAW(type) type;
+
+#endif /* !defined(_INC_CRT_UNICODE_MACROS) */
